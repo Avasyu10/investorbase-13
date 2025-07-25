1d2c7726924b42bbbb84ec34d97ca6d959879e9e
Avasyu10 <124695867+Avasyu10@users.noreply.github.com>
2025-07-25 12:28:45 +0530

    Update CompanyInfoCard.tsx

--- src/components/companies/CompanyInfoCard.tsx
+++ src/components/companies/CompanyInfoCard.tsx
@@ -294,7 +294,6 @@ export function CompanyInfoCard({
               className="h-10 px-4 bg-amber-400 hover:bg-amber-300 text-slate-950 border-amber-600"
             >
               <Bot className="h-4 w-4 mr-2" />
-              
             </Button>
           )}
 
